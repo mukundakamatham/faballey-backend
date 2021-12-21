5689a57f50395fc96d1eb33e992ddec45a4ae3bc
mukundakamatham <mukundakamatham@gmail.com>
2021-12-21 14:52:32 +0530

    update

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const app = require("./index")
 
 const connect = require("./configs/db")
-
-app.listen(2345, async function () {
+require("dotenv").config();
+const port = process.env.PORT || 2345 ;
+app.listen(port, async function () {
     await connect();
-    console.log("listening on port 2345  ");
+    console.log(`listening on port on ${port} `);
     console.log('  \\{^_^}/ hi!')
 })
 //$ git push . HEAD:BACKEND
