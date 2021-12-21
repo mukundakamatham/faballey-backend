d007ce95da285a03ba590d4dcef863501ba5d257
mukundakamatham <mukundakamatham@gmail.com>
2021-12-21 15:09:57 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ const cartController = require("./controllers/cart.controller");
 
 
 const app = express();
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+} 
 
 app.use(express.json({ extended: false }));
 
